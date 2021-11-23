b205d2d5fb4440afe1ee5e2b0cf512e8cd81337c
saawan1309 <58839883+saawan1309@users.noreply.github.com>
2021-11-23 14:59:46 +0530

    Update problem 4.py

--- problem 4.py
+++ problem 4.py
@@ -12,3 +12,13 @@ for i in range(1,10):
 for i in range(1,10):
     print(f"{i}:{b[i-1]}",end=" ")
 
+# second approch
+a = list(map(int,input().split()))
+count = {}
+for i in range(1,10):
+    c=0
+    for j in a:
+        if j%i==0:
+            c+=1
+        count[i]=c
+print(count)
